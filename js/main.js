7d97801176889ca2ff2d0ee8fb2d7d7d42aeafb9
Samuel Ogu <hello@samuelogu.com>
2024-04-23 17:49:13 +0100

    working code

--- js/main.js
+++ js/main.js
@@ -4,10 +4,16 @@ let eqn = document.getElementById('eqn');
 let del = document.getElementById('delete')
 let resultBox = document.getElementById('result');
 
+let newResult = false;
+
 for (let index = 0; index < num.length; index++) {
     const element = num[index];
     element.addEventListener('click', () => {
         let value = element.value;
+        if (newResult) {
+            newResult = false
+            return input(value, true);
+        }
         input(value)
     })
 }
@@ -16,6 +22,10 @@ for (let index = 0; index < operator.length; index++) {
     const element = operator[index];
     element.addEventListener('click', () => {
         let value = element.value;
+        if (newResult) {
+            newResult = false
+            return input(value, true);
+        }
         input(value)
     })
 }
@@ -24,25 +34,31 @@ eqn.addEventListener('click', calculate)
 del.addEventListener('click', backspace)
 
 function calculate() {
-    console.log('calculate');
+    const inputBox = resultBox.innerHTML;
+    const result = eval(inputBox)
+    input(result, true)
+    newResult = true;
 }
 
 function backspace() {
+
+    if (newResult) {
+        newResult = false
+        return input('', true);
+    }
+
     const inputBox = resultBox.innerHTML;
     const newInput = inputBox.slice(0, -1);
     input(newInput, true);
+
 }
 
 function input(value, clear = false) {
-    // if (clear) {
-    //     resultBox.innerHTML = value
-    // }else {
-    //     resultBox.innerHTML += value
-    // }
-    // if (clear) {
-    //     return resultBox.innerHTML = value
-    // }
-    // resultBox.innerHTML += value
+    const inputBox = resultBox.innerHTML;
+
+    if (inputBox === '' && value === '0') {
+        return;
+    }
 
     clear ? resultBox.innerHTML = value : resultBox.innerHTML += value
 }
